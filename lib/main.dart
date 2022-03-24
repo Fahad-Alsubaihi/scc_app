d0be55bbf0ea66fa028a2fa815f001ba522629c1
fahad-almalik <69044074+fahad-almalik@users.noreply.github.com>
2022-03-24 14:04:42 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,411 @@
+
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'home.dart';
+import 'tasks.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
 
-  // This widget is the root of your application.
+
+
+class WaveShape1 extends CustomClipper<Path> {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  getClip(Size size) {
+    double height = size.height;
+    double width = size.width;
+    var p = Path();
+    p.lineTo(0, height);
+    //p.cubicTo(width * 1 / 2, 0, width * 2 / 4, height, width, 0);
+    p.cubicTo(100 , 140, 130, 70, 130 , 60);
+    p.lineTo(width, 0);
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    p.close();
+    return p;
   }
 
   @override
+  bool shouldReclip(CustomClipper oldClipper) => false;
+}
+
+
+class WaveShape2 extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    double height = size.height;
+    double width = size.width;
+    var p = Path();
+    p.lineTo(0, 135);
+    //p.cubicTo(width * 1 / 2, 0, width * 2 / 4, height, width, 0);
+    p.cubicTo(150 , 20, 250, 150, 360 , 0);
+    p.lineTo(width, 0);
+
+    p.close();
+    return p;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => false;
+}
+
+class WaveShape3 extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    double height = size.height;
+    double width = size.width;
+    var p = Path();
+    p.lineTo(0, height);
+    //p.cubicTo(width * 1 / 2, 0, width * 2 / 4, height, width, 0);
+    p.cubicTo(100 , 160, 140, 120, 110 , 70);
+    p.lineTo(width, 0);
+
+    p.close();
+    return p;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => false;
+}
+
+class WaveShape4 extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    double height = size.height;
+    double width = size.width;
+    var p = Path();
+    p.lineTo(0, 100);
+    //p.cubicTo(width * 1 / 2, 0, width * 2 / 4, height, width, 0);
+    p.cubicTo(100 , 80, 220, 160, 360 , 0);
+    p.lineTo(width, 0);
+
+    p.close();
+    return p;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => false;
+}
+
+class WaveShape5 extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    double height = size.height;
+    double width = size.width;
+    var p = Path();
+    p.lineTo(0, height);
+    //p.cubicTo(width * 1 / 2, 0, width * 2 / 4, height, width, 0);
+    p.cubicTo(30 , 10, 360, 120, 360 , 0);
+    p.lineTo(width, 0);
+
+    p.close();
+    return p;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => false;
+}
+
+class Home extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: Colors.blueGrey[50],
+      body:
+      Stack(
+        clipBehavior: Clip.none,
+        children: [
+          ClipPath(
+            clipper: WaveShape5(),
+            child: Container(
+              width: 360,
+              height: 50,
+              color: Colors.blueAccent,
+            ),
+          ),
+          ClipPath(
+            clipper: WaveShape4(),
+            child: Container(
+              width: 360,
+              height: 125,
+              color: Colors.blueAccent[100],
+            ),
+          ),
+          ClipPath(
+            clipper: WaveShape3(),
+            child: Container(
+              width: 360,
+              height: 145,
+              color: Colors.blueAccent,
+            ),
+          ),
+          ClipPath(
+            clipper: WaveShape1(),
+            child: Container(
+              width: 360,
+              height: 145,
+              color: Colors.blue[800],
+            ),
+          ),
+          ClipPath(
+            clipper: WaveShape2(),
+            child: Container(
+              width: 360,
+              height: 125,
+              color: Colors.blue[800],
+            ),
+          ),
+          Positioned(
+            top: 30,
+            left: 10,
+            child: Image(
+              image: AssetImage(
+                  'assets/Logo.jpg'),
+              width: 70.0,
+              height: 70.0,
+            ),
+          ),
+          Positioned(
+            top: 25,
+            left: 150,
+            child: Text(
+              'My Accounts',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 170,
+            left: 30,
+            child:Container(
+              //margin: EdgeInsets.only(left: 30, top: 100, right: 30, bottom: 50),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.white.withOpacity(1),
+                    spreadRadius: 10,
+                    blurRadius: 0,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+                  borderRadius: BorderRadius.all(Radius.circular(10))
+              ),
+              //margin: EdgeInsets.symmetric(vertical: 70, horizontal: 15),
+              width: 350,
+              height: 200,
+              //color: Colors.grey[100],
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Text(
+                      "mohammed almotlaq",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 17,
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "441103283",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Icon(Icons.face),
+                      SizedBox(
+                        width: 20,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "0537485947",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Icon(Icons.phone),
+                      SizedBox(
+                        width: 20,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "toto@hotmail.com",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Icon(Icons.email),
+                      SizedBox(
+                        width: 20,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.mode_edit),
+                      Text(
+                        "tasks : 5",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 15,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      Text(
+                        "point : 30",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 15,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 110,
+            right: -40,
+            child: Container(
+              width: 100.0,
+              height: 100.0,
+              decoration: BoxDecoration(
+                color: const Color(0xff7c94b6),
+                image: DecorationImage(
+                  image: AssetImage('assets/MAQ.jpeg'),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                border: Border.all(
+                  color: Colors.white,
+                  width: 6.0,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 162,
+            left: 25,
+            child: Image(
+              image: AssetImage(
+                  'assets/hhh.jpg'),
+              width: 70.0,
+              height: 70.0,
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        child: Container(
+          height: 60.0,
+          color: Colors.white,
+          child: Row(
+
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                   Positioned(
+                      child: Image(
+                        image: AssetImage(
+                            'assets/user.png'),
+                        width: 0,
+                        height: 0,
+                      ),
+                    ),
+                    Positioned(
+                      left: -165,
+                      bottom: -23,
+                      child: Image(
+                        image: AssetImage(
+                            'assets/user.png'),
+                        width: 45,
+                        height: 45,
+                      ),
+                    ),
+                    Positioned(
+                      left: 120,
+                      bottom: -23,
+                      child: Image(
+                        image: AssetImage(
+                            'assets/medal.png'),
+                        width: 45,
+                        height: 45,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Container(
+          height: 80.0,
+          width: 80.0,
+          child: FittedBox(
+            child: FloatingActionButton(onPressed: () {},
+              child: Icon(
+              Icons.home_outlined,
+              size: 40,
+            ),
+            ),
+          ),
+        )
     );
   }
 }
